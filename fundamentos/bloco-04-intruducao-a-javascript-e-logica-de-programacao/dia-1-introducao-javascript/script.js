17e83ffb9b7b0170275afb21da5433fb8accf6d8
Matheus Almeida Saporito <matheus_cs.nf@hotmail.com>
2022-03-07 19:41:36 -0300

    exercicio

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/script.js
@@ -0,0 +1,184 @@
+/* const myName = "Matheus";
+const birthCity = "novaFatima";
+let birthYear = 2030;
+console.log(birthYear); */
+
+/* let patientId = "50";
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+ */
+
+
+
+
+
+/* const base = 5;
+let height = 8;
+
+const area = base * height;
+console.log(area)
+
+const perimeter = 4;
+console.log(perimeter) */
+
+/* const nota = 40;
+
+
+
+
+
+if(nota == 80){
+    console.log("Parabéns, você foi aprovada(o)");
+}
+else if( nota < 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera");
+}
+    
+ else {
+   console.log("Você foi reprovada(o)");} */
+
+
+
+
+
+
+  /*  const comida = 'pão na chapa';
+   const bebida = 'cafézinho';
+   
+   if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+     console.log('Muito obrigado pela refeição :)');
+   } else {
+     console.log('Acho que houve um engano com meu pedido');
+   }
+
+   const conditionOne = true;
+   const conditionTwo = false;
+ 
+   console.log(conditionOne && conditionTwo);
+ */
+
+
+
+
+
+
+
+/* 
+   const cenouras = true;
+   const leite = true;
+   const arroz = true;
+   const feijao = true;
+   
+   const listaDeCompras = cenouras && leite && arroz && feijao; 
+   resultadoAcima = true*/
+
+
+  /*  console.log(true && true); // true
+   console.log(true && false); // false
+   console.log(false && true); // false
+   console.log(false && false); // false
+   resultadoAbaixo = false */
+
+
+
+ /*   const currentHour = 12;
+   let message = "";
+
+   if (currentHour >= 22){
+       message = "Não deveríamos comer nada, é hora de dormir"; 
+        
+   }
+   else if (currentHour >= 18 && currentHour < 22){
+       message = "Rango da noite, vamos jantar :D";
+       
+
+   }
+   else if (currentHour >= 14 && currentHour <18){
+       messagem = "Vamos fazer um bolo pro café da tarde?";
+       
+   }
+   else if (currentHour >= 11 && currentHour < 14){
+       message = "Hora do almoço!!!";
+       
+   }
+   else if (currentHour >= 4 && currentHour < 11){
+        message = "Hmmm, cheiro de café recém passado";
+        
+   }
+
+   console.log(message) */
+
+
+
+
+
+
+ /*   const bebidaPrincipal =  'cafezinho';
+   const bebidaAlternativa = 'suco de laranja';
+   
+   if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+     console.log("Obrigado por me atender :D")
+   } else {
+     console.log("Ei, eu não pedi isso!");} */
+
+
+
+/* console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false */
+
+
+/* let weekDay = "quarta-feira"
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" |weekDay === "quarta-feira" |weekDay === "quinta-feira" |weekDay === "sexta-feira"){
+    console.log( "Oba, mais um dia de aprendizado na Trybe >:D");
+
+}
+else{
+    console.log("FINALMENTE, descanso merecido UwU");
+}
+   */ 
+
+
+
+
+/* let nota2 = 60;
+
+switch (nota2){
+    case nota2 = 90:
+    console.log("aprovado");
+    break;
+
+    case nota2 = 60:
+    console.log("lista");
+    break;
+
+    case nota2 < 59:
+    console.log("reprovado");
+    break;
+
+    default:
+    console.log("valor não identificado");
+
+} */
+
+
+
+
+   
+
+
+
+
