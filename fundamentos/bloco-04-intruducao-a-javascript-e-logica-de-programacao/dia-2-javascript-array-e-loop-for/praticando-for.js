72d4c13280a5300895ae4ede0160e11ceab5cbec
Matheus Almeida Saporito <matheus_cs.nf@hotmail.com>
2022-03-08 16:06:31 -0300

    praticando exercicios array, for, for/of

---
+++ fundamentos/bloco-04-intruducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/praticando-for.js
@@ -0,0 +1,8 @@
+// Exercício 1
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+/* let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let cont = 0; cont < groceryList.length - 1; cont += 1){
+    console.log(groceryList[cont]);
+} */
