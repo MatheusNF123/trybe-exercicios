4e19bb72d238bb2d88ec821f473896665bf61526
Matheus <matheus_cs.nf@hotmail.com>
2022-04-03 13:25:56 -0300

    exericio praticando explicaçoes

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/praticando explicaçoes/arrow functions.js
@@ -0,0 +1,19 @@
+const printName = () => {
+    const myName = 'Lucas';
+    return myName;
+  };
+  
+  console.log(printName());
+
+  //////////////////////////////
+  const printNome = () => 'Lucas';
+console.log(printNome());
+
+///////////////////////////////////
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
+
+
+////////////////////////////////////////////////////////////////
+const multiplication = (number, multi) => number * multi;
+console.log(multiplication(8, 2));
