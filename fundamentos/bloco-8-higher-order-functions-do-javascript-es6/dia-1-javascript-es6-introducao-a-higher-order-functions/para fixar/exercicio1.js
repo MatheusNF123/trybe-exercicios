a3f30a8901e336821ed3ad933b16a23fade170f0
Matheus <matheus_cs.nf@hotmail.com>
2022-04-08 23:13:25 -0300

    exercicios 8.1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/para fixar/exercicio1.js
@@ -0,0 +1,7 @@
+const string1 = () =>  console.table('Acordado')
+const string2 = () =>  console.log('Bora tomar café!')
+const string3 = () =>  console.log('Partiu dormir!!')
+
+const doingThings = (fun1) => fun1()
+
+doingThings(string2)
