1ed1ff91c3bd937af55a980316c77ae05e1a6825
Matheus <matheus_cs.nf@hotmail.com>
2022-05-05 21:13:54 -0300

    exercicio 1 e 2

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+// const Task = ({value}) => {
+//   return (
+//     <li>{value}</li>
+//   )
+// }
+
+const tarefas = ['acordar', 'jogar', 'tomar cafe', 'almoçar', 'tomar banho'] 
+
+function App() {
+  const task = (value) => {
+    return (
+      <li key={value}>{value}</li>
+    )
+  }
+  
+  return (
+    <div className="App">
+      <h1>Hello, World</h1> 
+      <div>
+        <ol>{tarefas.map(elemento => ( task(elemento)))}</ol>
+      </div>
+      {/* {task('Acordar!')} */}
+      {/* <Task value={'Acordar'}/> */}
+    </div>
+  );
+}
+
+export default App;
